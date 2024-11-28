8877c7b65065cbb63e34752df1a12b660d00bc71
Joel Obowu <57295185+bedbugs-bit@users.noreply.github.com>
2024-11-28 22:04:47 +0200

    added solution for doata2_senate.py solution using a queue data structure

---
+++ queue/doata2_senate.py
@@ -0,0 +1,30 @@
+from collections import deque
+
+class Solution:
+    def predictPartyVictory(self, senate: str) -> str:
+        n = len(senate)
+
+        r_queue = deque()
+        d_queue = deque()
+
+        for index, senator in enumerate(senate):
+            if senator == 'R':
+                r_queue.append(index)
+            else:
+                d_queue.append(index)
+
+        # simulate the rounds between both types of senators
+        while r_queue and d_queue:
+            r_index = r_queue.popleft()
+            d_index = d_queue.popleft()
+
+            if r_index < d_index:
+                r_queue.append(r_index + n)
+            else:
+                d_queue.append(d_index + n)
+
+        return "Radiant" if r_queue else "Dire"
+
+
+
+
