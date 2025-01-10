6deaa4292201e2f2fb5949be682b7920d754337b
Joel Obowu <57295185+bedbugs-bit@users.noreply.github.com>
2025-01-11 00:32:44 +0300

    add code for Longest_zig_zag_pattern.py

---
+++ trees/Longest_zig_zag_pattern.py
@@ -0,0 +1,39 @@
+# Definition for a binary tree node.
+from typing import Optional
+
+
+class TreeNode:
+    def __init__(self, val, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+        
+
+class Solution:
+    def __init__(self):
+        self.max_zigzag = 0
+
+    def longestZigZag(self, root: Optional[TreeNode]) -> int:
+        
+
+        
+        def dfs(node, is_left, length):
+            if not node:
+                return 0
+            
+            self.max_zigzag = max(self.max_zigzag, length)
+
+            if is_left:
+                dfs(node.left, False, length + 1)
+                dfs(node.right, True, 1)
+
+            else:
+                dfs(node.right, True, length + 1)
+                dfs(node.left, False, 1)
+
+        dfs(root, True, 0)
+        dfs(root, False, 0)
+
+        return self.max_zigzag
+            
+            
