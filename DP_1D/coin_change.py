7f0a17a0707911c7ce974f6ea7994811211eff7e
Joel Obowu <57295185+bedbugs-bit@users.noreply.github.com>
2025-02-13 06:00:46 +0300

    update code for DSA using greedy algo

---
+++ DP_1D/coin_change.py
@@ -0,0 +1,16 @@
+from typing import List
+class Solution:
+    def coinChange(self, coins: List[int], amount: int) -> int:
+        size = amount + 1
+        dp = [float('inf')] * size
+        dp[0] = 0
+
+        for target in range(1, size):
+            for coin in coins:
+                if (target - coin) >= 0:
+                    dp[target] = min(dp[target], dp[target-coin] + 1)
+
+        return dp[amount] if dp[amount] != float('inf') else -1
+
+
+
